6aad8e21770abface1b24f064e48754cf364523c
nandabasilio <anandabasilio24@gmail.com>
2025-05-15 14:27:28 +0000

    ajustando

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,91 +1,43 @@
 #include <stdio.h>
 
-#define TAMANHO_TABULEIRO 10
-#define TAMANHO_NAVIO 3
-#define MAR 0
+#define TAM 10
+#define TAM_NAVIO 3
+#define AGUA 0
 #define NAVIO 3
 
-// Função para inicializar o tabuleiro com valor 0 (água)
-void inicializarTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
-    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
-        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
-            tabuleiro[i][j] = MAR;
-        }
-    }
-}
+int main() {
+    int tabuleiro[TAM][TAM];
+    int i, j;
 
-// Função para verificar se é possível posicionar o navio sem sair do tabuleiro e sem sobreposição
-int podePosicionarNavio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO],
-                        int linha, int coluna, char orientacao) {
-    if (orientacao == 'H') { // Horizontal
-        if (coluna + TAMANHO_NAVIO > TAMANHO_TABULEIRO)
-            return 0;
-        for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[linha][coluna + i] != MAR)
-                return 0;
-        }
-    } else if (orientacao == 'V') { // Vertical
-        if (linha + TAMANHO_NAVIO > TAMANHO_TABULEIRO)
-            return 0;
-        for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[linha + i][coluna] != MAR)
-                return 0;
-        }
-    }
-    return 1;
-}
+    // Inicializa o tabuleiro com água (0)
+    for (i = 0; i < TAM; i++)
+        for (j = 0; j < TAM; j++)
+            tabuleiro[i][j] = AGUA;
 
-// Função para posicionar um navio na matriz do tabuleiro
-void posicionarNavio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO],
-                     int linha, int coluna, char orientacao) {
-    if (orientacao == 'H') {
-        for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            tabuleiro[linha][coluna + i] = NAVIO;
-        }
-    } else if (orientacao == 'V') {
-        for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            tabuleiro[linha + i][coluna] = NAVIO;
-        }
-    }
-}
+    // Posiciona o navio 1 (horizontal) em (1,1)
+    int l1 = 1, c1 = 1;
+    for (i = 0; i < TAM_NAVIO; i++)
+        tabuleiro[l1][c1 + i] = NAVIO;
 
-// Função para exibir o tabuleiro no console
-void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
-    printf("Tabuleiro:\n\n");
-    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
-        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
-            printf("%d ", tabuleiro[i][j]);
+    // Posiciona o navio 2 (vertical) em (4,5)
+    int l2 = 4, c2 = 5;
+    for (i = 0; i < TAM_NAVIO; i++)
+        tabuleiro[l2 + i][c2] = NAVIO;
+
+    // Exibe cabeçalho das colunas (letras)
+    printf("   ");
+    for (j = 0; j < TAM; j++)
+        printf(" %c", 'A' + j);
+    printf("\n");
+
+    // Exibe o tabuleiro com números nas linhas e valores 0 e 3
+    for (i = 0; i < TAM; i++) {
+        printf("%2d ", i + 1);  // Número da linha (1 a 10)
+        for (j = 0; j < TAM; j++) {
+            printf(" %d", tabuleiro[i][j]);  // Mostra 0 (água) ou 3 (navio)
         }
         printf("\n");
     }
-}
-
-int main() {
-    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
-
-    // Inicializa o tabuleiro com água
-    inicializarTabuleiro(tabuleiro);
-
-    // Coordenadas e orientações dos navios
-    int linhaNavio1 = 2, colunaNavio1 = 3; // Navio 1 - horizontal
-    int linhaNavio2 = 5, colunaNavio2 = 7; // Navio 2 - vertical
-
-    // Verifica e posiciona o primeiro navio
-    if (podePosicionarNavio(tabuleiro, linhaNavio1, colunaNavio1, 'H')) {
-        posicionarNavio(tabuleiro, linhaNavio1, colunaNavio1, 'H');
-    } else {
-        printf("Erro ao posicionar o navio 1.\n");
-    }
-
-    // Verifica e posiciona o segundo navio
-    if (podePosicionarNavio(tabuleiro, linhaNavio2, colunaNavio2, 'V')) {
-        posicionarNavio(tabuleiro, linhaNavio2, colunaNavio2, 'V');
-    } else {
-        printf("Erro ao posicionar o navio 2.\n");
-    }
-
-    // Exibe o tabuleiro final com os navios posicionados
-    exibirTabuleiro(tabuleiro);
 
     return 0;
 }
